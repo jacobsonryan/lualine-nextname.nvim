9aa4d7a8d3bedd02cdad6942eea7e744dfb16393
jacobsonryan <ryanhjacobson@gmail.com>
2025-04-22 13:14:33 -0400

    chore: remove brackets

--- lua/lualine_components/nextname.lua
+++ lua/lualine_components/nextname.lua
@@ -8,18 +8,7 @@ local function bold(str)
   return "%#StatusLineBold#" .. str .. "%*"
 end
 
-local function format_label(left, right, use_brackets)
-  if use_brackets then
-    return string.format("[%s] %s", left, right)
-  else
-    return string.format("%s ➜ %s", left, right)
-  end
-end
-
-return function(opts)
-  opts = opts or {}
-  local use_brackets = opts.use_brackets == true
-
+return function()
   local filepath = vim.api.nvim_buf_get_name(0)
   local filename = vim.fn.fnamemodify(filepath, ":t")
   local ext = vim.fn.fnamemodify(filepath, ":e")
@@ -27,14 +16,14 @@ return function(opts)
 
   if filename == "page.tsx" or filename == "page.jsx" then
     local folder = capitalize(vim.fn.fnamemodify(filepath, ":h:t"))
-    return bold(icon .. " " .. format_label(folder, filename, use_brackets))
+    return bold(icon .. " " .. folder .. " ➜ " .. filename)
   end
 
   if filename == "route.ts" or filename == "route.js" then
     local normalized = filepath:gsub("\\", "/")
     local api_path = normalized:match("app/(api/.-)/route%.%a+$")
     if api_path then
-      return bold(format_label(api_path, filename, use_brackets))
+      return bold(api_path .. " ➜ " .. filename)
     end
   end
 
